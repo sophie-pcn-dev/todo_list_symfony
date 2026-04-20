905c4d6c7d961ea9c8f7e10c1998480a6e685001
Sophie P <poncinsophie.dev@gmail.com>
2026-04-20 11:20:14 +0200

    ajout d'une nouvelle tâche

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,21 +3,46 @@
 namespace App\Controller;
 
 use App\Repository\TaskRepository;
+use App\Form\TaskType;
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TaskController extends AbstractController
 {
-    #[Route('/', name: 'app_task')]
-    public function index(TaskRepository $repo): Response
+    #[Route('/', name: 'task_index')]
+    public function index(
+        Request $request, 
+        TaskRepository $repo, 
+        EntityManagerInterface $em): Response //em envoie vers bdd
     {
+
+
+        $task = new Task;
+        $form = $this->createForm(TaskType::class, $task);
+        $form-> handleRequest($request);
+
+        if($form->isSubmitted()){
+            $task->setIsDone(false);
+            $task->setCreatedAt(new \DateTimeImmutable());
+            $em->persist($task);
+            $em->flush();
+
+           return $this->redirectToRoute('task_index'); //quand on insère des données on redirige pour éviter le renvoi de form plusieurs fois
+        }
+     
+
         // $tasks = $repo->findAll(); // recup la liste des tâches
         // dd($tasks);
         // d = dump and die : c'est le var dump 
         return $this->render('task/index.html.twig', [
 
-            'tasks' => $repo->findAll()
+            'tasks' => $repo->findBy([], ['createdAt' => 'DESC']),
+            'form' => $form,
+            
         
            
         ]);
---
+++ src/Form/TaskType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder //champs du formulaire
+            ->add('title')
+            // ->add('isDone')
+            // ->add('createdAt', null, [
+            //     'widget' => 'single_text',
+            // ])
+            ->add('save', SubmitType::class, ['label' => 'ajouter une tâche'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
